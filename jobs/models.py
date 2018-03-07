afe8abe7741d738359305c3a6a557ee543fb0614
colin <dm2241@columbia.edu>
2018-03-07 08:03:54 -0500

    templates, login screen

--- jobs/models.py
+++ jobs/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 
 class Person(models.Model):
+    email = models.CharField(max_length=124)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email_secondary = models.CharField(max_length=256)
@@ -34,16 +35,20 @@ class Employer(models.Model):
     website = models.CharField(max_length=1024)
     contact_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.contact_person.first_name + ' ' + self.contact_person.last_name + ' (' + \
+            self.contact_person.email + ')'
+
 
 class Job(models.Model):
     title = models.CharField(max_length=1024, default=("Still to come " + str(timezone.now())), null=False)
     description = models.TextField(default="Still to come", null=models.NOT_PROVIDED)
     start_date = models.DateField(verbose_name='Start date')
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE, null=models.NOT_PROVIDED)
-    job_specific_contact = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+    job_specific_contact = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.title + ' starts ' + self.start_date
+        return self.title + ' starts ' + str(self.start_date)
 
 
 class Question(models.Model):
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Job
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    jobs = Job.objects.order_by('start_date')[:5]
+    # output = ', '.join([('<a href="./' + str(j.id) + '">' + j.title + '</a>') for j in jobs])
+    template = loader.get_template('jobs/index.html')
+    context = {
+        'jobs': jobs,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, job_id):
+    job = Job.objects.get(id=job_id)
+    output = '' + str(job)
+    for attr, value in job.__dict__.items():
+        # output += ' ' + str(attr) + ' '
+        output += '<div class="job ' + attr + '">' + attr + ': ' + str(value) + '</div>'
+    return HttpResponse(output)
+
